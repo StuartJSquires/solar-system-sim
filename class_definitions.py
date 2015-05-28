ebb31039f181c0a4affd584edb44c1e119acb305
StuartJSquires <StuartJSquires@users.noreply.github.com>
2015-05-28 18:00:19 -0400

    Added some files to start working on

---
+++ class_definitions.py
@@ -0,0 +1,11 @@
+class Body(object):
+    """This is the class used for all bodies. We might want to make subclasses
+    for different types of bodies at some point in the future.
+
+    Attributes:
+        mass:
+
+    """
+
+    def __init__(self):
+        """This function is called when the object is created"""
