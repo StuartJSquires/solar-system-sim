7a5d2da450272ba794f992873dba63074d6f0c65
StuartJSquires <StuartJSquires@users.noreply.github.com>
2015-06-05 17:34:11 -0400

    Can now init body objects from input files

--- class_definitions.py
+++ class_definitions.py
@@ -1,11 +1,26 @@
+from read_input import *
+
 class Body(object):
     """This is the class used for all bodies. We might want to make subclasses
     for different types of bodies at some point in the future.
 
     Attributes:
-        mass:
-
+        name: the name of the body
+        parent: the name of the body that the orbital parameters of this body 
+            are given relative to
+        mass: the mass of the body
     """
 
-    def __init__(self):
-        """This function is called when the object is created"""
+    def __init__(self, filename):
+        """This function takes the list of body parameters from the input files
+        and uses them to create the body object with appropriate attributes.
+
+        Args:
+            filename: the name of the input file to use to create the body
+        """
+
+        body_params = read_body(filename)
+
+        self.name = body_params["name"]
+        self.parent = body_params["parent"]
+        self.mass = body_params["mass"]
---
+++ parameters.py
@@ -0,0 +1,19 @@
+# Relevant files
+
+INITIAL_CONDITION_DIRECTORY = "input_data"
+OUTPUT_DIRECTORY = "output_directory"
+OUTPUT_FILE_BASE = "snapshot"
+
+
+# Run Characteristics
+
+BEGIN_TIME = 0.0
+MAX_TIME = 0.0
+MAX_TIME_STEP = 0.0
+OUTPUT_TIME_STEP = 0.0
+TIME_OF_FIRST_OUTPUT = 0.0
+
+
+# Units
+
+GRAVITATIONAL_CONSTANT = 6.67
