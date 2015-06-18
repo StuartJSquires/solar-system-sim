402e132ba0fceb23abbf710b500a14835c35ac35
StuartJSquires <StuartJSquires@users.noreply.github.com>
2015-06-17 20:58:10 -0400

    Bodies now init with proper position and magnitude

--- class_definitions.py
+++ class_definitions.py
@@ -13,7 +13,7 @@ class Body(object):
         mass: the mass of the body
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename, **params):
         """This function takes the list of body parameters from the input files
         and uses them to create the body object with appropriate attributes.
 
@@ -21,26 +21,79 @@ class Body(object):
             filename: the name of the input file to use to create the body
         """
 
+        G = params["GRAVITATIONAL_CONSTANT"]
         body_params = read_body(filename)
 
         self.name = body_params["name"]
         self.parent = body_params["parent"]
         self.mass = body_params["mass"]
-        self.semimajor = body_params["semimajor"]
-        self.eccentricity = body_params["eccentricity"]
-        self.direction = body_params["direction"]
-        self.start = body_params["start"]
+
+        if self.parent not None:
+            self.semimajor = body_params["semimajor"]
+            self.eccentricity = body_params["eccentricity"]
+            self.argument_of_periapsis = body_params["argument_of_periapsis"]
+            self.inclination = body_params["inclination"]
+            self.ascending_node_longitude = body_params["ascending_node_longitude"]
+            self.direction = body_params["direction"]
+            self.start = body_params["start"]
 
         if self.parent == None:
             self.velocity = np.zeros(3)
             self.position = np.zeros(3)
         else:
+            a = self.semimajor
+            e = self.eccentricity
+
             if self.start == "periapsis":
-                velocity_magnitude = np.sqrt(((1 + eccentricity) * G * M) / ((1 - e) * a)
-                position_magnitude = semimajor * (1 - eccentricity)
+                velocity_magnitude = np.sqrt(((1 + e) * G * M) / ((1 - e) * a)
+                position_magnitude = semimajor * (1 - e)
             else if self.start == "apoapsis":
-                velocity_magnitude = np.sqrt(((1 - eccentricity) * G * M) / ((1 + e) * a)
-                position_magnitude = semimajor * (1 + eccentricity)
+                velocity_magnitude = np.sqrt(((1 - e) * G * M) / ((1 + e) * a)
+                position_magnitude = semimajor * (1 + e)
             else:
                 print "FATAL ERROR: INVALID START POSITION FOR", self.name
                 sys.exit("Stopping program.")
+
+                angle_1 = self.ascending_node_longitude
+                angle_2 = self.inclination
+                angle_3 = self.argument_of_periapsis
+
+                position_x_direction = ((np.cos(angle_1) * 
+                                         np.cos(angle_2) * 
+                                         np.cos(angle_3)) - 
+                                        (np.sin(angle_1) * 
+                                         np.sin(angle_2))
+
+                position_y_direction = ((np.cos(angle_1) * 
+                                         np.sin(angle_3)) +
+                                        (np.cos(angle_2) *
+                                         np.cos(angle_3) *
+                                         np.sin(angle_1)))
+
+                position_z_direction = np.cos(angle_3) * np.sin(angle_2)
+
+                position_direction = np.asarray([position_x_direction,
+                                                 position_y_direction,
+                                                 position_z_direction])
+
+                self.position = position_magnitude * position_direction
+
+                velocity_x_direction = (-(np.cos(angle_3) * 
+                                          np.sin(angle_1)) -
+                                        (np.cos(angle_1) *
+                                         np.cos(angle_2) *
+                                         np.sin(angle_3)))
+
+                velocity_y_direction = ((np.cos(angle_1) * 
+                                         np.cos(angle_3)) -
+                                         np.cos(angle_2) * 
+                                         np.sin(angle_1) *
+                                         np.sin(angle_3))
+
+                velocity_z_direction = -np.sin(angle_2) * np.sin(angle_3)
+
+                velocity_direction = np.asarray([velocity_x_direction,
+                                                 velocity_y_direction,
+                                                 velocity_z_direction])
+
+                self.velocity = velocity_magnitude * position_magnitude
