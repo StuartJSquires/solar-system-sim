d547c48e2ef57774ec85b83bcc25fcb74e3e8eb3
Thurteen <dylan.johnston.1994@gmail.com>
2015-06-23 19:35:51 -0400

    Began timestep function

--- Timestep.py
+++ Timestep.py
@@ -1,2 +1,12 @@
-for bodies in gravitationally_interacting_bodies
-	
+def verlet_step(interacting_bodies):
+
+	for body in interacting_bodies:
+		position = np.copy(body.position)
+		
+		for interacting_body in interacting_bodies:
+			if interacting_body != body:
+				second_position = np.copy(interacting_body.position)
+				
+				distance = position - second_position
+
+				
